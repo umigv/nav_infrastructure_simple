d405024f89a7a44069721bbea8af01368d2a8fb0
Ethan Hardy <102838865+hardy-ethan@users.noreply.github.com>
2025-12-03 19:40:27 -0500

    docs: document goal selection node file

--- goal_selection/goal_selection/node.py
+++ goal_selection/goal_selection/node.py
@@ -12,25 +12,39 @@ from rclpy.node import Node, Publisher
 from pyproj import Transformer
 
 def degrees_to_radians(degrees: float) -> float:
+    """Convert degrees to radians."""
     return math.pi/180 * degrees
 
 
 def lat_long_to_meters(latitude: float, longitude: float) -> tuple[float, float]:
-    # WGS84 to UTM (automatically selects zone)
+    """
+    Convert latitude and longitude from degrees to meters.
+    
+    This conversion process depends on which "meters" coordinate system you use.
+    
+    TODO: It's correctness should be validated.
+    """
     transformer = Transformer.from_crs("EPSG:4326", "EPSG:32618", always_xy=True)
     return transformer.transform(longitude, latitude)
 
 
+# Waypoints are constant goals, supplied by the competition organizer
 WAYPOINTS_FILE_PATH = pathlib.Path("/home/umarv/ros2_ws/src/nav_infrastructure_simple/goal_selection/waypoints.json")
+# How often a path is generated and published, in seconds.
 PATH_PUBLISH_PERIOD_SECONDS = 2
 
 class GoalSelectionNode(Node):
+    # The most recent odometry for the robot
     odometry: Odometry | None = None
+    # The closest waypoint to the robot in robot relative coordinates
     waypoint_robot_relative: Point | None = None
+    # The most recent occupancy grid, inflated by the inflation layer node
     inflated_occupancy_grid: OccupancyGrid | None = None
+    # The publisher for generated paths
     path_publisher: Publisher
 
     def __init__(self):
+        """Initialize goal selection node."""
         super().__init__('goal_selection')
 
         self.create_subscription(
@@ -63,9 +77,17 @@ class GoalSelectionNode(Node):
         self.create_timer(PATH_PUBLISH_PERIOD_SECONDS, self.generate_and_publish_path)
 
     def odometry_callback(self, new_odometry: Odometry):
+        """Store odometry data into member variable."""
         self.odometry = new_odometry
 
     def gps_callback(self, new_gps_data: NavSatFix):
+        """
+        Consumes the latest GPS data to find the robot relative coordinates of the
+        competition waypoints.
+
+        TODO: Currently always choses the first waypoint in the list, but should choose
+        the logically next waypoint.
+        """
         if self.odometry is None:
             return
 
@@ -86,9 +108,11 @@ class GoalSelectionNode(Node):
         self.get_logger().info(f"Robot relative waypoint: {self.waypoint_robot_relative}")
 
     def inflated_occupancy_grid_callback(self, new_occupancy_grid: OccupancyGrid):
+        """Store latest inflated occupancy grid."""
         self.inflated_occupancy_grid = new_occupancy_grid
 
     def generate_and_publish_path(self):
+        """Generate and publish path for robot to follow."""
         if self.inflated_occupancy_grid is None or self.odometry is None or self.waypoint_robot_relative is None:
             return
 
@@ -105,6 +129,7 @@ class GoalSelectionNode(Node):
 
 
 def main(args=None):
+    """Node entrypoint."""
     rclpy.init(args=args)
 
     goal_selection = GoalSelectionNode()
