dbbbf311b69f3488e57336eb316ddbcf43d3f3c3
Ryan Liao <ryan.liao0305@gmail.com>
2026-01-19 13:16:35 -0500

    robot_localization_fusion

---
+++ localization/setup.py
@@ -0,0 +1,27 @@
+from glob import glob
+from setuptools import find_packages, setup
+
+package_name = 'localization'
+
+setup(
+    name=package_name,
+    version='0.1.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', ['launch/launch.py']),
+    ],  
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ryanliao',
+    maintainer_email='ryanliao@umich.edu',
+    description='Localization helper nodes',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        "console_scripts": [
+            "gps_origin_initializer = localization.gps_origin_initializer:main",
+        ],
+    },
+)
