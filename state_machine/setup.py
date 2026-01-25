ead4cb3eecac35fc8266ff7fe0fd219a0c62956b
Ryan4253 <ryan.liao0305@gmail.com>
2026-01-25 22:23:13 +0000

    Fill out metadata

--- state_machine/setup.py
+++ state_machine/setup.py
@@ -4,7 +4,7 @@ package_name = 'state_machine'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.1.0',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
@@ -13,13 +13,14 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='umarv',
-    maintainer_email='ryan.liao0305@gmail.com',
-    description='TODO: Package description',
+    maintainer='Ryan Liao',
+    maintainer_email='ryanliao@umich.edu',
+    description='ROS2 state machine for publishing robot operating modes and handling state transitions',
     license='Apache-2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'state_machine = state_machine.state_machine:main',
         ],
     },
 )
