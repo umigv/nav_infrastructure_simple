adb833518a45a3125d2214a0f81ed56af686ae54
Ryan4253 <ryan.liao0305@gmail.com>
2026-01-27 00:55:29 +0000

    Make gps_publisher use config loader

---
+++ gps_publisher/gps_publisher/gps_publisher_config.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass
+
+@dataclass(frozen=True)
+class GpsPublisherConfig:
+    serial_port: str = '/dev/ttyACM0'
+    poll_rate_hz: float = 100.0
+    gps_frame_id: str = "gps_link"
+
+    def __post_init__(self):
+        if self.poll_rate_hz <= 0:
+            raise ValueError("GpsPublisherConfig: poll_rate_hz must be > 0")
