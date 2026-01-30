56b55c77cb457bcea5be1dbeb96b765717e365a1
Ryan4253 <ryan.liao0305@gmail.com>
2026-01-30 09:10:05 +0000

    Post init validation

--- occupancy_grid_transform/occupancy_grid_transform/occupancy_grid_trasform_config.py
+++ occupancy_grid_transform/occupancy_grid_transform/occupancy_grid_trasform_config.py
@@ -6,6 +6,21 @@ class InflationParams:
     inflation_falloff_radius_cells: int = 20
     inflation_decay_factor: float = 0.9
 
+    def __post_init__(self):
+        if self.inflation_radius_cells < 0:
+            raise ValueError("InflationParams: inflation_radius_cells must be > 0")
+        
+        if self.inflation_falloff_radius_cells < 0:
+            raise ValueError("InflationParams: inflation_falloff_radius_cells must be > 0")
+        
+        if self.inflation_radius_cells > self.inflation_falloff_radius_cells:
+            raise ValueError("InflationParams: inflation_falloff_radius_cells must be >= inflation_radius_cells")
+
+        if not (0 < self.inflation_decay_factor < 1):
+            raise ValueError("InflationParams: inflation_decay_factor must be between 0 and 1")
+        
+
+
 @dataclass(frozen=True)
 class OccupancyGridTransformConfig:
     inflation_params: InflationParams
