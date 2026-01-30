4dcba3a6e18dd5ef9239d403aedcffd86e025611
Ryan4253 <ryan.liao0305@gmail.com>
2026-01-30 08:28:48 +0000

    Fix downstream

--- occupancy_grid_transform/setup.py
+++ occupancy_grid_transform/setup.py
@@ -4,22 +4,23 @@ package_name = 'occupancy_grid_transform'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.1.0',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=['setuptools', 'nav_utils'],
+    install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='umarv',
-    maintainer_email='ryan.liao0305@gmail.com',
-    description='TODO: Package description',
+    maintainer='Ryan Liao',
+    maintainer_email='ryanliao@umich.edu',
+    description='Occupancy grid transform and inflation layer',
     license='Apache-2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            "occupancy_grid_transform = occupancy_grid_transform.occupancy_grid_transform:main",
         ],
     },
 )
