5acddd12001cbc3f9d9f3e426f67f0e95a490704
Ryan4253 <ryan.liao0305@gmail.com>
2026-02-10 17:04:38 +0000

    Teleop

---
+++ teleop/launch/teleop.launch.py
@@ -0,0 +1,63 @@
+from __future__ import annotations
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+pkg_share = get_package_share_directory("teleop")
+
+controller_config = {
+    "ps4": os.path.join(pkg_share, "config", "teleop_ps4.yaml"),
+    "xbox": os.path.join(pkg_share, "config", "teleop_xbox.yaml"),
+}
+
+
+def _launch_setup(context, *args, **kwargs):
+    controller = LaunchConfiguration("controller").perform(context).strip().lower()
+    if controller not in controller_config:
+        raise RuntimeError(f"Unknown controller '{controller}'. Expected one of: {', '.join(controller_config.keys())}")
+
+    config_file = controller_config[controller]
+    joystick_dev = LaunchConfiguration("joystick_dev").perform(context).strip()
+
+    return [
+        Node(
+            package="joy",
+            executable="joy_node",
+            name="joystick_node",
+            output="screen",
+            parameters=[
+                config_file,
+                {"dev": joystick_dev},
+            ],
+        ),
+        Node(
+            package="teleop_twist_joy",
+            executable="teleop_node",
+            name="teleop_twist_joy_node",
+            output="screen",
+            parameters=[config_file],
+            remappings=[("cmd_vel", "teleop_cmd_vel")],
+        ),
+    ]
+
+
+def generate_launch_description() -> LaunchDescription:
+    return LaunchDescription(
+        [
+            DeclareLaunchArgument(
+                "controller",
+                description=f"Controller profile: {', '.join(controller_config.keys())}",
+            ),
+            DeclareLaunchArgument(
+                "joystick_dev",
+                default_value="/dev/input/js0",
+                description="Device of joystick",
+            ),
+            OpaqueFunction(function=_launch_setup),
+        ]
+    )
--- teleop/setup.py
+++ teleop/setup.py
@@ -1,25 +1,26 @@
 from setuptools import find_packages, setup
+from glob import glob
 
-package_name = 'teleop'
+package_name = "teleop"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.1.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        ('share/' + package_name + '/launch', glob('launch/*.py')),
+        ('share/' + package_name + '/config', glob('config/*.yaml')),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='umarv',
-    maintainer_email='ryan.liao0305@gmail.com',
-    description='TODO: Package description',
-    license='Apache-2.0',
-    tests_require=['pytest'],
+    maintainer="Ryan Liao",
+    maintainer_email="ryanliao@umich.edu",
+    description="Teleop control and velocity mux node",
+    license="Apache-2.0",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-        ],
+        "console_scripts": [],
     },
 )
