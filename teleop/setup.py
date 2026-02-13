204a7c5c5a6e727907410b32100cc573a318a77e
Ryan Liao <71594512+Ryan4253@users.noreply.github.com>
2026-02-12 20:27:55 -0500

    Add teleop package (#33)
    Closes #30 
    
    ## What has changed
    Added a teleop and twist mux package for field testing
    
    ## Testing plan
    Tested joystick mapping and mux behavior locally with xbox controller

---
+++ teleop/setup.py
@@ -0,0 +1,27 @@
+from glob import glob
+
+from setuptools import find_packages, setup
+
+package_name = "teleop"
+
+setup(
+    name=package_name,
+    version="0.1.0",
+    packages=find_packages(exclude=["test"]),
+    data_files=[
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        ("share/" + package_name + "/launch", glob("launch/*.py")),
+        ("share/" + package_name + "/config", glob("config/*.yaml")),
+    ],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="Ryan Liao",
+    maintainer_email="ryanliao@umich.edu",
+    description="Teleop control and velocity mux node",
+    license="Apache-2.0",
+    tests_require=["pytest"],
+    entry_points={
+        "console_scripts": [],
+    },
+)
