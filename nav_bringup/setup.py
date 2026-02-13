66def7ade631d2fee8ee6db00a563d53280b8fb3
Ryan Liao <ryan.liao0305@gmail.com>
2026-02-13 17:58:21 -0500

    Add vectornav submodule
    Add submodule init to setup script
    
    Launch file WIP
    
    Use python file for global config
    
    Fill out package and setup
    
    Rename to imu_publisher
    
    IMU and GPS Config
    
    use imu raw
    
    Format
    
    Switch to robotnik vectornav package
    
    Remove old package
    
    Switch to https for git submodule, add dialout group to setup, make test not use setup script
    
    Remove dialout comment in gps pub
    
    Correct configs and transform
    
    0p7
    
    delete helper
    
    change covariance

--- nav_bringup/setup.py
+++ nav_bringup/setup.py
@@ -7,18 +7,19 @@ package_name = "nav_bringup"
 
 setup(
     name=package_name,
-    version="0.0.0",
+    version="0.1.0",
     packages=find_packages(exclude=["test"]),
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
         (os.path.join("share", package_name, "launch"), glob("launch/*")),
+        (os.path.join("share", package_name, "config"), glob("config/*")),
     ],
     install_requires=["setuptools"],
     zip_safe=True,
-    maintainer="arv",
-    maintainer_email="102838865+hardy-ethan@users.noreply.github.com",
-    description="TODO: Package description",
+    maintainer="Ethan Hardy",
+    maintainer_email="hardyem@umich.edu",
+    description="Launch files and shared configuration for the navigation stack",
     license="Apache-2.0",
     extras_require={},
     entry_points={
