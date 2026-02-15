4785a319855614bdb92dc69bdce20cd90cafc7a3
Ryan Liao <ryan.liao0305@gmail.com>
2026-02-15 04:20:24 -0500

    Create path planning package, move path planning impl

---
+++ path_planning/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+package_name = 'path_planning'
+
+setup(
+    name=package_name,
+    version='0.1.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Caitlyn Trievel',
+    maintainer_email='ctrievel@umich.edu',
+    description='A* path planning',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
