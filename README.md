c482e4aa2890c5e3d1503a7156ad2728f4f6b288
Ryan Liao <ryan.liao0305@gmail.com>
2026-02-14 20:40:10 -0500

    Add sim to launch file and update docs

--- README.md
+++ README.md
@@ -3,16 +3,17 @@
 ## Documentation
 
 ## Warning: Intellisense
-When building, make sure you include `--symlink-install`. Otherwise, when control clicking a dependency in VSCode, it will take you to a copy of the dependency, instead of the actual source file. For example:
+When building, make sure you include `--symlink-install`. Otherwise, when control clicking a dependency in VSCode, it 
+will take you to a copy of the dependency, instead of the actual source file. For example:
 
 ```bash
 colcon build --symlink-install
 ```
 
 ### Dependencies
-This repo only has one dependecy: `pyproj`. You can install it with
+You can install all dependencies of nav by running
 ```bash
-pip install pyproj
+./scripts/setup.sh
 ```
 
 ### Simulation
@@ -21,7 +22,8 @@ You can run simulation by:
 2. Running this stack with simulation enabled
 3. Publishing initial gps coordinates
 
-The following describes how to run the point simulator (basic non-physics based simulator with an empty occupancy grid). Run each of these commands in separate terminals.
+The following describes how to run the point simulator (basic non-physics based simulator with an empty occupancy grid). 
+Run each of these commands in separate terminals.
 
 1. Publish an empty occupancy grid repeatedly:
     ```bash
@@ -36,15 +38,13 @@ The following describes how to run the point simulator (basic non-physics based 
         orientation: {x: 0.0, y: 0.0, z: 0.0, w: 1.0}
     data: [$(python3 -c 'print(", ".join(["0"]*100*100))')]"
     ```
-2. Run navigation stack with simulation enabled:
+2. Run simulated localization:
     ```bash
-    ros2 launch nav_bringup infra.launch.py simulation:=true
+    ros2 launch nav_bringup localization.launch.py simulation:=true
     ```
-3. Publish initial gps coords
+3. Run nav stack:
     ```bash
-    ros2 topic pub /gps_coords sensor_msgs/msg/NavSatFix "{header: {frame_id: 'gps'}, status: {status: 0, service: 1}, latitude: 42.294621, longitude: -83.708112, altitude: 10.0, position_covariance: [0.0,0.0,0.0, 0.0,0.0,0.0, 0.0,0.0,0.0], position_covariance_type: 0}" --once
+    ros2 launch nav_bringup infra.launch.py
     ```
-
-Feel free to modify the initial gps coords. 
 
 Keep in mind that full occupancy grid simulation is likely infeasible, since the occupancy grid needs to "turn" with the robot.
--- nav_bringup/launch/localization.launch.py
+++ nav_bringup/launch/localization.launch.py
@@ -1,11 +1,16 @@
 from launch import LaunchDescription
-from launch.substitutions import PathJoinSubstitution
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition, UnlessCondition
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 from nav_bringup.global_config import FRAMES
 
 
 def generate_launch_description():
+    sim_arg = DeclareLaunchArgument("sim", default_value="false", description="Run in simulation mode")
+    sim = LaunchConfiguration("sim")
+
     bringup_share = FindPackageShare("nav_bringup")
     localization_params = PathJoinSubstitution([bringup_share, "config", "localization.yaml"])
 
@@ -14,6 +19,7 @@ def generate_launch_description():
         executable="ekf_node",
         name="ekf_local",
         output="screen",
+        condition=UnlessCondition(sim),
         parameters=[
             localization_params,
             {
@@ -33,6 +39,7 @@ def generate_launch_description():
         executable="navsat_transform_node",
         name="navsat_transform",
         output="screen",
+        condition=UnlessCondition(sim),
         parameters=[localization_params],
         remappings=[
             ("imu", "imu/raw"),
@@ -47,6 +54,7 @@ def generate_launch_description():
         executable="ekf_node",
         name="ekf_global",
         output="screen",
+        condition=UnlessCondition(sim),
         parameters=[
             localization_params,
             {
@@ -66,14 +74,31 @@ def generate_launch_description():
         executable="gps_origin_initializer",
         name="gps_origin_initializer",
         output="screen",
+        condition=UnlessCondition(sim),
         remappings=[("gps", "gps/raw"), ("set_datum", "datum")],
+    )
+
+    localization_simulator = Node(
+        package="localization_simulator",
+        executable="localization_simulator",
+        name="localization_simulator",
+        condition=IfCondition(sim),
+        parameters=[
+            {
+                "map_frame_id": FRAMES["map_frame"],
+                "odom_frame_id": FRAMES["odom_frame"],
+                "base_frame_id": FRAMES["base_frame"],
+            },
+        ],
     )
 
     return LaunchDescription(
         [
+            sim_arg,
             ekf_local,
             navsat,
             ekf_global,
             gps_origin_initializer,
+            localization_simulator,
         ]
     )
