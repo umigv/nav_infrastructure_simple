db8471733e5e0706ccf0117d051dee2c19315032
Ryan Liao <71594512+Ryan4253@users.noreply.github.com>
2026-02-15 16:32:21 -0500

    Make localization simulator match the intended localization interface (#39)

--- nav_bringup/launch/infra.launch.py
+++ nav_bringup/launch/infra.launch.py
@@ -1,24 +1,10 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.conditions import IfCondition
-from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    simulation_arg = DeclareLaunchArgument(
-        "simulation", default_value="false", description="Whether to run in simulation mode"
-    )
-
     return LaunchDescription(
         [
-            simulation_arg,
-            Node(
-                package="point_simulator",
-                executable="point_simulator",
-                name="point_simulator",
-                condition=IfCondition(LaunchConfiguration("simulation")),
-            ),
             Node(package="goal_selection", executable="goal_selection", name="goal_selection"),
             Node(
                 package="occupancy_grid_transform",
