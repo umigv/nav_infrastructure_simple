4d85d2c734bc25dcbbb648c2bafa5222d1c1eca8
Ryan Liao <ryan.liao0305@gmail.com>
2026-02-15 04:07:15 -0500

    Fix edge cases and bugs for path planning

--- goal_selection/goal_selection/path_planning.py
+++ goal_selection/goal_selection/path_planning.py
@@ -1,15 +1,7 @@
 from dataclasses import dataclass, field
-import dataclasses
-import itertools
-import json
-import math
-import sys
 from collections import deque
 import heapq
-from nav_msgs.msg import OccupancyGrid, Path
-from geometry_msgs.msg import Point, Pose, PoseStamped, Quaternion
-from rclpy.node import Node
-from std_msgs.msg import Header
+from geometry_msgs.msg import Point
 from nav_utils.nav_utils.geometry import distance
 from nav_utils.nav_utils.world_occupancy_grid import WorldOccupancyGrid 
 
@@ -20,7 +12,11 @@ class KeyAndCost:
     key: int = field(compare=False)
 
 
-def find_closest_drivable_point(grid: WorldOccupancyGrid, robot_position: Point) -> Point | None:
+def find_closest_drivable_point(
+    grid: WorldOccupancyGrid, 
+    robot_position: Point, 
+    max_search_radius: float
+) -> Point | None:
     """
     The robot is in unknown space in the occupancy grid, meaning we don't know if its
     current location and the location around it is drivable. 
@@ -30,20 +26,23 @@ def find_closest_drivable_point(grid: WorldOccupancyGrid, robot_position: Point)
 
     Used to traverse the area between the robot's real position and where its FoV begins.
     """
+    if grid.state(robot_position).is_drivable:
+        return robot_position
+
     visited: set[int] = {grid.hash_key(robot_position)}
     search_container: deque[Point] = deque([robot_position])
     
     while len(search_container) > 0:
         current = search_container.popleft()
 
-        # Start by searching forwards, then forwards-left, forwards-right, and finally
-        # just left and right. Searching backwards is not necessary because all nodes
-        # behind the robot are of unknown value.
         for neighbor in grid.neighbors_forward(current):
             neighor_key = grid.hash_key(neighbor)
             if neighor_key in visited:
                 continue
             visited.add(neighor_key)
+
+            if distance(neighbor, robot_position) > max_search_radius:
+                continue
 
             if grid.state(neighbor).is_driveable:
                 return neighbor
@@ -53,15 +52,16 @@ def find_closest_drivable_point(grid: WorldOccupancyGrid, robot_position: Point)
     
     return None
 
-def generate_path(grid: WorldOccupancyGrid, start: Point, goal: Point):
+def generate_path(grid: WorldOccupancyGrid, start: Point, goal: Point) -> list[Point] | None:
     """
     Generate a good path for the robot to follow towards the goal using the A* search
     algorithm https://en.wikipedia.org/wiki/A*_search_algorithm.
-    
-    Uses occupancy grid indices as the coordinate system.
     """
     start_key = grid.hash_key(start)
     goal_key = grid.hash_key(goal)
+
+    if start_key == goal_key:
+        return [start]
 
     point_of: dict[int, Point] = {start_key: start}
     came_from: dict[int, int | None] = {start_key: None}
@@ -84,7 +84,7 @@ def generate_path(grid: WorldOccupancyGrid, start: Point, goal: Point):
             break
 
         for neighbor in grid.neighbors8(current_point):
-            if grid.state(neighbor).is_drivable:
+            if not grid.state(neighbor).is_drivable:
                 continue
 
             neighbor_key = grid.hash_key(neighbor)
@@ -94,12 +94,12 @@ def generate_path(grid: WorldOccupancyGrid, start: Point, goal: Point):
                 cost_so_far[neighbor_key] = neighbor_cost
                 came_from[neighbor_key] = current_key
                 point_of[neighbor_key] = neighbor
-                # A* priority: cost_so_far + heuristic (distance to waypoint)
                 priority = neighbor_cost + distance(neighbor, goal)
                 heapq.heappush(priority_queue, KeyAndCost(priority, neighbor_key))
 
-    # In order to construct a path from the search process, start from the best node
-    # within the occupancy grid, and work backwards until you reach the start node. 
+    if best_goal_key == start_key:
+        return None
+
     backtrace: list[Point] = []
     current_key: int | None = best_goal_key
     while current_key != None:
