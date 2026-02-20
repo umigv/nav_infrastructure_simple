ea1495bb9cf13c9e7be28a3c9b5e6832298ac13b
Ryan Liao <ryan.liao0305@gmail.com>
2026-02-19 23:37:17 -0500

    Use math for rotation

--- nav_bringup/launch/sensors.launch.py
+++ nav_bringup/launch/sensors.launch.py
@@ -1,3 +1,5 @@
+import math
+
 from launch import LaunchDescription
 from launch.substitutions import PathJoinSubstitution
 from launch_ros.actions import Node
@@ -32,55 +34,43 @@ def generate_launch_description():
         ],
     )
 
+    # fmt: off
     tf_base_to_imu = Node(
         package="tf2_ros",
         executable="static_transform_publisher",
         name="tf_base_to_imu",
         output="screen",
         arguments=[
-            "--x",
-            "0.0",
-            "--y",
-            "0.0",
-            "--z",
-            "0.0",
-            "--roll",
-            "0.0",
-            "--pitch",
-            "0.0",
-            "--yaw",
-            "-1.57079632679",
-            "--frame-id",
-            FRAMES["base_frame"],
-            "--child-frame-id",
-            FRAMES["imu_frame"],
+            "--x", "0.0",
+            "--y", "0.0",
+            "--z", "0.0",
+            "--roll", "0.0",
+            "--pitch", "0.0",
+            "--yaw", str(-math.pi / 2),
+            "--frame-id", FRAMES["base_frame"],
+            "--child-frame-id", FRAMES["imu_frame"],
         ],
     )
+    # fmt: on
 
+    # fmt: off
     tf_base_to_gps = Node(
         package="tf2_ros",
         executable="static_transform_publisher",
         name="tf_base_to_gps",
         output="screen",
         arguments=[
-            "--x",
-            "0.0",
-            "--y",
-            "0.0",
-            "--z",
-            "0.8128",
-            "--roll",
-            "0.0",
-            "--pitch",
-            "0.0",
-            "--yaw",
-            "0.0",
-            "--frame-id",
-            FRAMES["base_frame"],
-            "--child-frame-id",
-            FRAMES["gps_frame"],
+            "--x", "0.0",
+            "--y", "0.0",
+            "--z", "0.8128",
+            "--roll", "0.0",
+            "--pitch", "0.0",
+            "--yaw", "0.0",
+            "--frame-id", FRAMES["base_frame"],
+            "--child-frame-id", FRAMES["gps_frame"],
         ],
     )
+    # fmt: on
 
     return LaunchDescription(
         [
