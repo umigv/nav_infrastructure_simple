3b7d5c14fd20850bada8bc3d50c23c1f3d88b872
Ryan Liao <ryan.liao0305@gmail.com>
2026-02-28 04:42:49 -0500

    Sensor sim

--- nav_bringup/nav_bringup/global_config.py
+++ nav_bringup/nav_bringup/global_config.py
@@ -5,3 +5,13 @@ FRAMES = {
     "odom_frame": "odom",
     "map_frame": "map",
 }
+
+# GPS origin in WGS84 degrees used for simulation. All map-frame coordinates are relative to this point.
+# Used by the localization simulator and other packages (e.g. course creation tools) that simulate a field.
+GPS_ORIGIN_SIM = {
+    "latitude": 42.294621,
+    "longitude": -83.708112,
+}
+
+# Magnetic declination at the deployment site (radians, west-negative).
+MAGNETIC_DECLINATION_RADIANS = -0.1269
